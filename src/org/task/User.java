ef06aa90f491084ac483ae43520322738e87b9d5
Prasannth-ramasamy <cme.prasanth@gmail.com>
2022-03-19 11:17:46 +0530

    new

--- src/org/task/User.java
+++ src/org/task/User.java
@@ -3,21 +3,22 @@ package org.task;
 import java.util.LinkedList;
 import java.util.List;
 
-public class User{public static void main(String[] args) {
-	List<Integer>li=new LinkedList<Integer>();
-	li.add(2);
-	li.add(1);
-	li.add(4);
-	li.add(3);
-	li.add(5);
+public class User {
+	public static void main(String[] args) {
+		List<Integer> li = new LinkedList<Integer>();
+		li.add(2);
+		li.add(1);
+		li.add(4);
+		li.add(3);
+		li.add(5);
 
-	for (int i = 0; i < li.size();i=i+2) {
-		
-		System.out.println(li.get(i));
-		System.out.println(li.get(i));
-		System.out.println("bala");
+		for (int i = 0; i < li.size(); i = i + 2) {
 
-		
-}
-}
+			System.out.println(li.get(i));
+			System.out.println(li.get(i));
+			System.out.println("bala");
+			System.out.println("Prasanth");
+
+		}
+	}
 }
